c580c6c3d43e03f86c3d2aeadb100d840f438c5d
RickWithNoK <ricescalante12@gmail.com>
2026-02-14 16:21:45 -0800

    Theme vars for media card and media details. Forgot to stage App.jsx  with the searchpage stuff

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -2,30 +2,33 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import SearchPage from '../pages/Search/SearchPage';
 import MediaDetailsPage from '../pages/MediaDetails/MediaDetailsPage';
 import LoginPage from '../pages/Login/LoginPage';
+import { ThemeProvider } from '../context/ThemeContext';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          {/* Default route redirects to search */}
-          <Route path="/" element={<Navigate to="/search" replace />} />
-          
-          {/* Login page */}
-          <Route path="/login" element={<LoginPage />} />
-          
-          {/* Search page - main landing */}
-          <Route path="/search" element={<SearchPage />} />
-          
-          {/* Media details page */}
-          <Route path="/media/:id" element={<MediaDetailsPage />} />
-          
-          {/* Catch-all route */}
-          <Route path="*" element={<Navigate to="/search" replace />} />
-        </Routes>
-      </div>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <div className="App">
+          <Routes>
+            {/* Default route redirects to search */}
+            <Route path="/" element={<Navigate to="/search" replace />} />
+            
+            {/* Login page */}
+            <Route path="/login" element={<LoginPage />} />
+            
+            {/* Search page - main landing */}
+            <Route path="/search" element={<SearchPage />} />
+            
+            {/* Media details page */}
+            <Route path="/media/:id" element={<MediaDetailsPage />} />
+            
+            {/* Catch-all route */}
+            <Route path="*" element={<Navigate to="/search" replace />} />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
